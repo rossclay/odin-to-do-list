e1a6d76a7579c6aa2e2f2803c84ab498e5b09c0c
Ross Clay <clay.rossclay@gmail.com>
2024-06-27 17:27:34 -0400

    initialize Task class

--- src/index.js
+++ src/index.js
@@ -1,2 +1,43 @@
-import './style.css';
-import sideBarImage from './img/'
+import { parse, format, isValid } from 'date-fns'
+
+class Task {
+    constructor(title, description, dueDate, priority) {
+        this.title = title
+        this.description = description
+        this.dueDate = dueDate
+        this.priority = priority
+    }
+
+    formatDate(date) {
+        if (date) {
+            const dateObject = parse(date, "yyyy-MM-dd", new Date())
+            if (isValid(dateObject)) {
+                return this.format(dateObject, 'MM/dd/yyyy')
+            }
+            return '';
+        }
+    }
+    updateTitle(newTitle) {
+        this.title = newTitle;
+    }
+
+    updateDescription(newDescription) {
+        this.description = newDescription;
+    }
+
+    updateDueDate(newDueDate) {
+        this.dueDate = this.formatDate(newDueDate);
+    }
+
+    updatePriority(newPriority) {
+        this.priority = newPriority;
+    }
+
+}
+
+
+class Project {
+    constructor(tasks) {
+        this.tasks
+    }
+}
