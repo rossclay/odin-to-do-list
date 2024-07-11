b5c0c33cbf41d389cc450ef6efca7764e427a1ca
Ross Clay <clay.rossclay@gmail.com>
2024-07-11 15:47:30 -0400

    adds task and project classes

---
+++ src/task.js
@@ -0,0 +1,87 @@
+import { parse, format, isValid } from 'date-fns'
+
+class Task {
+    constructor(title, description, dueDate, priority) {
+        this.title = title
+        this.description = description
+        this.dueDate = dueDate
+        this.priority = priority
+    }
+
+    formatDate(date) {
+        if (date) {
+            const dateObject = parse(date, "yyyy-MM-dd", new Date())
+            if (isValid(dateObject)) {
+                return this.format(dateObject, 'MM/dd/yyyy')
+            }
+            return '';
+        }
+    }
+    updateTitle(newTitle) {
+        this.title = newTitle;
+    }
+
+    updateDescription(newDescription) {
+        this.description = newDescription;
+    }
+
+    updateDueDate(newDueDate) {
+        this.dueDate = this.formatDate(newDueDate);
+    }
+
+    updatePriority(newPriority) {
+        this.priority = newPriority;
+    }
+
+    toJSON() {
+        return {
+            title: this.title,
+            description: this.description,
+            dueDate: this.dueDate,
+            priority: this.priority,
+            complete: this.complete,
+            id: this.id
+        };
+    }
+
+    static fromJSON(json) {
+        const item = new Task(json.title, json.description, json.dueDate, json.priority);
+        item.complete = json.complete;
+        item.id = json.id;
+        item.dueDate = json.dueDate;
+        return item;
+    }
+}
+
+
+const createTask = (title, description, dueDate, priority) => {
+    return new Task(title, description, dueDate, priority);
+};
+
+
+class Project {
+    constructor(name) {
+        this.id = Date.now().toString()
+        this.name
+        this.taskList = []
+    }
+
+    addTask(task) {
+        this.taskList.push(task)
+    }
+
+    removeTask(task) {
+        const index = this.taskList.indexOf(task)
+        if (index < -1) {
+            this.taskList.splice(index, 1)
+        }
+
+    }
+}
+
+const createProject = (name) => {
+    const myProject = new Project(name)
+
+}
+
+export default { createTask, Task, createProject, Project };
