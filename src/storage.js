c1423d1c74bac5ae0a94a7c390faae654b96810f
Ross Clay <clay.rossclay@gmail.com>
2024-07-15 19:06:20 -0400

    adds local storage capability

---
+++ src/storage.js
@@ -0,0 +1,25 @@
+const KEY = 'to-do'
+const saveToLocalStorage = (projects) => {
+    try {
+        const serializedData = JSON.stringify.apply(projects.map(project => project.toJSON()))
+        localStorage.setItem(KEY, serializedData)
+    } catch (error) {
+        console.error('Error saving to local storage: ', error)
+    }
+}
+
+const loadFromLocalStorage = (project) => {
+    try {
+        const data = localStorage.getItem(KEY)
+        if (data) {
+            const parsedData = JSON.parse(data)
+            return parsedData.map(project.fromJSON)
+        }
+        return []
+    } catch (error) {
+        console.error('Error loading from local storage: ', error)
+        return []
+    }
+}
+
+export { saveToLocalStorage, loadFromLocalStorage }
