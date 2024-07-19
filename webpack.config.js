87bc69160f01340473551fd0e54e60a2198fbf50
Ross Clay <clay.rossclay@gmail.com>
2024-07-19 16:45:26 -0400

    corrects project title

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
-            title: 'Restaurant Page',
+            title: 'To-do List',
             filename: 'index.html',
             inject: 'body',
         })],
