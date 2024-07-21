ad6ac2da681d8999dc39644b55414db937c9fbfa
Ross Clay <clay.rossclay@gmail.com>
2024-07-21 09:41:10 -0400

    adds strikethrough text for completed tasks

--- src/UIControls.js
+++ src/UIControls.js
@@ -2,22 +2,15 @@ import { format, transpose } from "date-fns";
 import { saveToLocalStorage } from "./storage";
 import writeImgSrc from "./img/icons8-write-48.png"
 import trashImgSrc from "./img/icons8-trash-24.png"
+import projectImgSrc from './img/icons8-project-32.png'
 import taskModule from "./task.js";
 
 
 const UIController = (() => {
-  // modal to allow the user to add tasks
-  const addTaskBtn = document.querySelector(".add-task-btn");
-  const closeBtn = document.querySelector(".close-modal");
-  const taskModal = document.querySelector(".task-modal");
-  const submitModalBtn = document.querySelector(".submit-modal-btn");
-  const content = document.querySelector(".content");
+  const cardSection = document.querySelector(".card-section");
 
-  addTaskBtn.addEventListener("click", () => taskModal.showModal());
-
-  closeBtn.addEventListener("click", () => taskModal.close());
-
-  submitModalBtn.addEventListener("click", () => {
+  // create new tasks
+  const createNewTask = () => {
     let newTaskTitle = document.querySelector("#task-title").value;
     let newTaskDescription = document.querySelector("#task-description").value;
     let newTaskDueDate = format(
@@ -34,8 +27,31 @@ const UIController = (() => {
     );
     // need to rework these from the library project to make it work here
     addTaskToProject(newTask);
+  }
+
+  // add srcs for images in sample tasks and projects
+  const writeImgs = document.querySelectorAll('.img-write')
+  writeImgs.forEach((image) =>
+    image.src = writeImgSrc)
+  const trashImgs = document.querySelectorAll('.img-trash')
+  trashImgs.forEach((image) =>
+    image.src = trashImgSrc)
+  const projectImgs = document.querySelectorAll('.img-project')
+  projectImgs.forEach((image) =>
+    image.src = projectImgSrc)
+
+  // modal functionality
+  const addTaskBtn = document.querySelector(".add-task-btn");
+  const closeBtn = document.querySelector(".close-modal");
+  const taskModal = document.querySelector(".task-modal");
+  const submitModalBtn = document.querySelector(".submit-modal-btn");
+  addTaskBtn.addEventListener("click", () => taskModal.showModal());
+  closeBtn.addEventListener("click", () => taskModal.close());
+
+  submitModalBtn.addEventListener("click", () => {
+    createNewTask()
     // need to create functions related to displaying tasks and reset projects?
-    resetProjects(content);
+    resetProjects(cardSection);
     displayTasks(myProjects);
     taskModal.close();
   });
@@ -54,38 +70,23 @@ const UIController = (() => {
 
   // responsive task cards: strike through tasks once they are complete
 
-  const strikeThruCompleteTasks = (existingTaskInformationHTML) => {
-    if (isTaskComplete) {
-      console.log('is any of this happening?')
-      let newTaskInformationHTML = "<s>" + existingTaskInformationHTML + "<s>";
-      return newTaskInformationHTML;
-    }
-  };
-
   const isTaskComplete = (event) => {
-    console.log(event.target.checked)
     if (event.target.checked) return true;
     else return false;
   };
 
   const taskBoxes = document.querySelectorAll(".task-checkbox");
   // use forEach to add an event listener to all of these
-  taskBoxes.forEach((taskBox) =>
-    taskBox.addEventListener("click", (event) => {
-      let existingTaskInformationHTML = event.target.innerHTML;
-      console.log(existingTaskInformationHTML)
-      event.target.innerHTML = strikeThruCompleteTasks(
-        existingTaskInformationHTML
-      );
+  taskBoxes.forEach((taskBox) => {
+    taskBox.addEventListener('click', (e) => {
+      if (isTaskComplete(e)) {
+        e.target.parentNode.classList.add('strike-through')
+      } else (e.target.parentNode.classList.remove('strike-through'))
     })
-  );
+  })
 
-  const writeImgs = document.querySelectorAll('.img-write')
-  writeImgs.forEach((image) =>
-    image.src = writeImgSrc)
-  const trashImgs = document.querySelectorAll('.img-trash')
-  trashImgs.forEach((image) =>
-    image.src = trashImgSrc)
+
+
 
 })();
 
