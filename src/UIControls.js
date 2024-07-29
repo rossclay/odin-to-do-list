9fad38674f4439b567caf5bbbdc68a9df4969470
Ross Clay <clay.rossclay@gmail.com>
2024-07-29 18:57:56 -0400

    adjusts task card creation

--- src/UIControls.js
+++ src/UIControls.js
@@ -32,18 +32,55 @@ const UIController = (() => {
     const newTaskCard = document.createElement("div");
     newTaskCard.classList.add("task-card");
     newTaskCard.classList.add(`${task.priority}`);
-    newTaskCard.innerHTML = `
-          <div class="task-header">
-            <input type="checkbox" class="task-checkbox" name="task-checkbox" />
-            <div class="task-content">
-              <div class="task-title">${task.title}</div>
-              <div class="task-description">${task.description}</div>
-            </div>
-          </div>
-          <div class="task-footer">
-            <div class="task-date">${task.dueDate}</div>
-            <div><img class="img-write" src="${writeImgSrc}"/><img class="img-trash" src="${trashImgSrc}"/></div>
-          </div>`;
+    // using creating elements instead of innerHTML to allow for easier editing of the task cards
+    let newTaskHeaderDiv = document.createElement("div");
+    newTaskHeaderDiv.classList.add("task-header");
+    let newTaskCheckbox = document.createElement("input");
+    newTaskCheckbox.classList.add("task-checkbox");
+    newTaskCheckbox.setAttribute("type", "checkbox");
+    newTaskCheckbox.setAttribute("name", "task-checkbox");
+    // dynamic checkbox functionality
+    newTaskCheckbox.addEventListener("click", (e) => {
+      handleTaskBoxClick(e);
+    });
+    newTaskHeaderDiv.appendChild(newTaskCheckbox);
+    let newTaskContentDiv = document.createElement("div");
+    newTaskContentDiv.classList.add("task-content");
+    newTaskHeaderDiv.appendChild(newTaskContentDiv);
+    let newTaskTitleDiv = document.createElement("div");
+    newTaskTitleDiv.classList.add("task-title");
+    newTaskTitleDiv.textContent = task.title;
+    newTaskContentDiv.appendChild(newTaskTitleDiv);
+    if (task.description) {
+      let newTaskDescriptionDiv = document.createElement("div");
+      newTaskDescriptionDiv.classList.add("task-description");
+      newTaskDescriptionDiv.textContent = task.description;
+      newTaskContentDiv.appendChild(newTaskDescriptionDiv);
+    }
+    let newTaskFooterDiv = document.createElement("div");
+    newTaskFooterDiv.classList.add("task-footer");
+    let newTaskDueDateDiv = document.createElement("div");
+    newTaskDueDateDiv.classList.add("task-date");
+    newTaskDueDateDiv.textContent = task.dueDate;
+    let imgContainer = document.createElement("div");
+    let newTaskWriteImg = document.createElement("img");
+    newTaskWriteImg.classList.add("img-write");
+    newTaskWriteImg.src = writeImgSrc;
+    newTaskWriteImg.addEventListener("click", () => {
+      handleEditTask(newTaskCard, task);
+    });
+    let newTrashImg = document.createElement("img");
+    newTrashImg.classList.add("img-trash");
+    newTrashImg.src = trashImgSrc;
+    newTrashImg.addEventListener("click", () => {
+      console.log("delete works");
+    });
+    imgContainer.appendChild(newTaskWriteImg);
+    imgContainer.appendChild(newTrashImg);
+    newTaskFooterDiv.appendChild(newTaskDueDateDiv);
+    newTaskFooterDiv.appendChild(imgContainer);
+    newTaskCard.appendChild(newTaskHeaderDiv);
+    newTaskCard.appendChild(newTaskFooterDiv);
     tasksSection.appendChild(newTaskCard);
   };
 
@@ -65,8 +102,12 @@ const UIController = (() => {
   closeBtn.addEventListener("click", () => taskModal.close());
 
   submitTaskModalBtn.addEventListener("click", () => {
-    createNewTaskCard(createNewTask());
-    taskModal.close();
+    try {
+      createNewTaskCard(createNewTask());
+      taskModal.close();
+    } catch {
+      alert("Task Title and Date are required!");
+    }
   });
   // additional feature for closing modal when clicking outside of dialog window
   taskModal.addEventListener("click", (e) => {
@@ -81,7 +122,95 @@ const UIController = (() => {
     }
   });
 
-  // project modal - UNDER CONSTRUCTION
+  // edit tasks - UNDER CONSTRUCTION
+  const handleEditTask = (taskCard, task) => {
+    // taskCard.innerHTML = `<div class="task-header">
+    //         <legend>Priority:</legend>
+    //         <select name="task-priority" id="task-priority">
+    //           <option value="high">high</option>
+    //           <option value="medium">medium</option>
+    //           <option value="low">low</option>
+    //         </select>
+    //         <label for="task-title"
+    //           >Task Title <span aria-label="required">*</span></label
+    //         >
+    //         <div class="task-content">
+    //           <input
+    //           class="task-input"
+    //           type="text"
+    //           id="task-title"
+    //           name="task-title"
+    //           value=${task.title}
+    //           required
+    //         />
+    //           <label for="task-description">Task Description</label>
+    //         <input
+    //           class="task-input"
+    //           type="text"
+    //           id="task-description"
+    //           name="task-description"
+    //           value=${task.description}
+    //           type="text"
+    //         />
+    //       </div>
+    //       <div class="task-footer">
+    //         <label for="task-date"
+    //           >Due date <span aria-label="required">*</span></label
+    //         >
+    //         <input type="date" id="task-date" name="task-date" value=${task.dueDate} required />
+    //         <div><img class="img-write" src="${writeImgSrc}"/><img class="img-trash" src="${trashImgSrc}"/></div>
+    //       </div>`;
+    const priorityLegend = document.createElement("legend");
+    priorityLegend.textContent = "Priority:";
+    const prioritySelect = document.createElement("select");
+    prioritySelect.setAttribute("name", "task-priority");
+    prioritySelect.setAttribute("id", "task-priority");
+    priorityLegend.appendChild(prioritySelect);
+    const optionHigh = document.createElement("option");
+    optionHigh.setAttribute("value", "high");
+    optionHigh.textContent = "high";
+    prioritySelect.appendChild(optionHigh);
+    const optionMedium = document.createElement("option");
+    optionMedium.setAttribute("value", "medium");
+    optionMedium.textContent = "medium";
+    prioritySelect.appendChild(optionMedium);
+    const optionLow = document.createElement("option");
+    optionLow.setAttribute("value", "low");
+    optionLow.textContent = "low";
+    prioritySelect.appendChild(optionLow);
+    // also need to update the js object
+    submitUpdateBtn.addEventListener("click", () => {
+      handleEditTaskUpdate();
+    });
+    cancelUpdateBtn.addEventListener("click", () => {
+      handleCancelTaskUpdate();
+    });
+  };
+
+  const handleEditTaskUpdate = (
+    task,
+    newTitle,
+    newDescription,
+    newDueDate,
+    newPriority
+  ) => {
+    task.updateTitle(newTitle);
+    task.updateDescription(newDescription);
+    task.updateDueDate(newDueDate);
+    task.updatePriority(newPriority);
+  };
+
+  const handleCancelTaskUpdate = () => {};
+
+  writeImgs.forEach((writeImg) => {
+    writeImg.addEventListener("click", (e) => {
+      let taskCard = e.target.parentNode.parentNode.parentNode;
+      console.log(taskCard);
+      // handleEditTask(taskCard);
+    });
+  });
+
+  // project modal
   const addProjectBtn = document.querySelector(".add-project-btn");
   const projectModal = document.querySelector("#project-modal");
   const toggleProjectModalVisibility = () => {
@@ -129,7 +258,6 @@ const UIController = (() => {
     if (event.target.checked) return true;
     else return false;
   };
-  // how can we add this functionality to NEW task checkboxes too?
   const taskBoxes = document.querySelectorAll(".task-checkbox");
   const handleTaskBoxClick = (e) => {
     if (isTaskComplete(e)) {
